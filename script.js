13680b947ad7d4d6a3d979611be4f3c47c0c84b5
LuizAgiota001 <145458499+LuizAgiota001@users.noreply.github.com>
2025-07-29 11:35:02 -0300

    v

--- script.js
+++ script.js
@@ -6,31 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enuciado:Você faz o que
-        alternativas: [
-            "pussy clean"
-            "pussy fresh"
-        ]
-    }
-    {
-         enuciado:Você faz o que
-        alternativas: [
-            "pussy clean"
-            "pussy fresh"
-        ]
-    },
-    {
-         enuciado:Você faz o que
-        alternativas: [
-            "pussy clean"
-            "pussy fresh"
+        enuciado: Você faz o que
+        alternativas: [ 
+            { 
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
+            {
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
         ]
     },
+   
     {
-         enuciado:Você faz o que
-        alternativas: [
-            "pussy clean"
-            "pussy fresh"
+        enuciado: Você faz o que
+        alternativas: [ 
+            { 
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
+            {
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
         ]
     },
-]
+   
+    {
+        enuciado: Você faz o que
+        alternativas: [ 
+            { 
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
+            {
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
+        ]
+    },
+   
+    {
+        enuciado: Você faz o que
+        alternativas: [ 
+            { 
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
+            {
+            texto: "pussy clean"
+            afirmacao: "afirmação"
+            },
+        ]
+    },
+   
+let atual =0;
+let perguntaATual;
+
+function mostraPergunta () {
+    perguntaAtual = pergunta [atual];
+    caixaPerguntas.textContent = perguntas.enunciado;
+}
+
+mostraPergunta ();
